5c6c231bb2976e36f2ee04075aec5bbd9c6b11b6
Avani Ausekar <94734661+avaniausekar@users.noreply.github.com>
2023-08-03 22:40:30 +0530

    Add files via upload

---
+++ hackathon/main.py
@@ -0,0 +1,110 @@
+from flask import Flask, render_template, request,flash,redirect,url_for,jsonify
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
+import os 
+app = Flask(__name__)
+app.debug = True  # Enable debug mode
+app.secret_key = 'your secret key'
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'food_donation'
+
+app.config['UPLOAD_FOLDER'] = 'static/uploads/'
+
+mysql = MySQL(app)
+
+
+# index page
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+
+@app.route('/login', methods =['GET', 'POST'])
+def login():
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if email and password:
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        select_query = "SELECT password FROM credentials WHERE email = %s"
+        cursor.execute(select_query, (email,))
+
+        result = cursor.fetchone()
+
+        if result and password == result['password']:
+            
+            return render_template("profile.html")
+        else:
+            
+            return "Invalid email or password", 401
+
+        cursor.close()
+        connection.close()
+
+    return render_template("login.html")
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    name = request.form.get("name")
+    email = request.form.get("email")
+    mobile = request.form.get("mobile")
+    address = request.form.get("address")
+    value = request.form.get("option")
+    pass1 = request.form.get("pass1")
+    pass2 = request.form.get("pass2")
+
+    if value == 'donor':
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT COUNT(*) AS row_count FROM donor')
+        result = cursor.fetchone()
+        num = result['row_count'] + 1
+        uid = "D" + str(num)
+        cursor.execute('INSERT INTO credentials VALUES \
+                (% s, % s, % s)',
+                       (uid, email,pass1))
+        cursor.execute('INSERT INTO donor VALUES \
+                (% s, % s, % s,%s)',
+                       (uid, name,mobile,address))
+        
+        mysql.connection.commit()
+        flash('Registration successful!', 'success')
+        return redirect('/login') 
+    if value == 'ngo':
+        file = request.files['image']
+        
+        
+        filename = file.filename
+        #file_path = os.path.join(os.getcwd(), filename)
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(file_path)
+
+        # Store the image, name, and email in the database
+        image_data = open(file_path, 'rb').read()
+
+
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT COUNT(*) AS row_count FROM ngo')
+        result = cursor.fetchone()
+        num = result['row_count'] + 1
+        uid = "N" + str(num)
+        cursor.execute('INSERT INTO credentials VALUES \
+                (% s, % s, % s)',
+                       (uid, email,pass1))
+        
+        cursor.execute('INSERT INTO ngo VALUES \
+                (% s, % s, % s,%s,%s)',
+                       (uid,name,mobile,address,image_data))
+        mysql.connection.commit()
+        #os.remove(file_path)
+        flash('Registration successful!', 'success')
+        return redirect('/login') 
+
+
+    return render_template("signup.html")
+
+if __name__ == '__main__':
+    app.run()
