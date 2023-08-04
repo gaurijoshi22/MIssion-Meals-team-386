ae54ed458ad82db9695ba20a805587e3e5ec4337
Avani Ausekar <94734661+avaniausekar@users.noreply.github.com>
2023-08-04 19:30:06 +0530

    retrieved donor donated data and some linking

--- hackathon/main.py
+++ hackathon/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request,flash,redirect,url_for,jsonify
+from flask import Flask, render_template, request,flash,redirect,url_for,jsonify,session
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
 import re
@@ -8,7 +8,7 @@ app.debug = True  # Enable debug mode
 app.secret_key = 'your secret key'
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'root123'
+app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'food_donation'
 
 app.config['UPLOAD_FOLDER'] = 'static/uploads/'
@@ -35,14 +35,23 @@ def login():
         result = cursor.fetchone()
 
         if result and password == result['password']:
-            
-            return render_template("donor.html")
+            select_query1 = "SELECT uid FROM credentials WHERE email = %s"
+            cursor.execute(select_query1, (email,))
+            result = cursor.fetchone()
+
+            session['uid'] = result
+            uid_val = result.get('uid')
+
+            if uid_val.startswith('D'):
+                return redirect('/donor_main') 
+            if uid_val.startswith('N'):
+                return redirect('/ngo_main') 
         else:
             
             return "Invalid email or password", 401
 
-        cursor.close()
-        connection.close()
+        mysql.cursor.close()
+        mysql.connection.close()
 
     return render_template("login.html")
 
@@ -106,36 +115,63 @@ def signup():
 
     return render_template("signup.html")
 
-@app.route('/scheduleEvent', methods=['GET', 'POST'])
-def scheduleEvent():
-    eventTitle = request.form.get("eventTitle")
-    eventDate = request.form.get("eventDate")
-    eventTime = request.form.get("eventTime")
-    contactName = request.form.get("contactName")
-    contactEmail = request.form.get("contactEmail")
-    eventDescription = request.form.get("eventDescription")
-    ngoReg = request.form.get("ngoReg")
-
-    #uid=session['user_id'] 
-
-    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-    #select_query = "SELECT uid FROM credentials WHERE uid = %s"
-    #cursor.execute(select_query, (uid,))
-
-    #result = cursor.fetchone()
-
-    select_query = "INSERT into Event values  (%s,%s,%s,%s,%s,%s,%s)"
-    cursor.execute(select_query, (ngoReg,eventTitle,eventDate,contactName,contactEmail,eventDescription,eventTime))
-    mysql.connection.commit()
-    flash('Event successful!', 'success')
-    return render_template("ngo.html")
-
-
-
 @app.route('/donor_main', methods=['GET', 'POST'])
 def donor_main():
-     return render_template("donor.html")
+    uid = session.get('uid')
+    uid_val = uid.get('uid')
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    select_query = "SELECT Uid FROM current_donations WHERE Uid = %s"
+    cursor.execute(select_query, (uid_val,))
+    result = cursor.fetchall()
+    if(result is not None):
+        select_query1 = "SELECT * FROM current_donations where Uid = %s"
+        cursor.execute(select_query1, (uid_val,))
+        data = cursor.fetchall()
+        da = []
+        for d in data:
+            da.append(d)
+    return render_template("donor.html",data = da)
 
+@app.route('/donor_main/donate_food', methods=['GET', 'POST'])
+def donateFood():
+    uid = session.get('uid')
+    uid_val = uid.get('uid')
+    if request.method == 'POST':
+        val = 0
+        item_name = request.form.get("item_name")
+        desc = request.form.get("desc")
+        qty = request.form.get("qty")
+        wt = request.form.get("wt")
+        pdate = request.form.get("pdate")
+        edate = request.form.get("edate")
+        status = "Available"
+
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('INSERT INTO current_donations VALUES \
+                    (%s,% s, % s, % s,%s,%s,%s,%s,%s)',
+                        (val,uid_val,item_name,desc,qty,wt,pdate,edate,status))
+        mysql.connection.commit()
+        flash('done', 'success')
+        return redirect("/donor_main")
+    return render_template("donate_food.html")
+
+    
+@app.route('/ngo_main', methods=['GET', 'POST'])
+def ngo_main():
+    uid = session.get('uid')
+    uid_val = uid.get('uid')
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    select_query = "SELECT ngo_name FROM ngo WHERE uid = %s"
+    cursor.execute(select_query, (uid_val,))
+    result = cursor.fetchone()
+
+    return render_template("ngo.html")
+
+@app.route('/logout')
+def logout():
+    session.pop('uid', None)
+    flash('Logged Out!', 'success')
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run()
