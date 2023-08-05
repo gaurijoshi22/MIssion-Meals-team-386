d919a82ad3fe3361ed330a9c9655925dd3f675e1
gaurijoshi22 <gauri.joshi@cumminscollege.in>
2023-08-05 14:27:55 +0530

    sql updated

--- food_donation.sql
+++ food_donation.sql
@@ -87,3 +87,32 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+CREATE TABLE `current_donations` (
+  `Donation_id` int(11) NOT NULL,
+  `Uid` varchar(255) NOT NULL,  `Item_name` varchar(40) NOT NULL,
+  `Item_description` varchar(255) NOT NULL,
+  `Item_quantity` int(11) NOT NULL,
+  `Item_weight` int(11) NOT NULL,
+  `Item_date_of_production` datetime NOT NULL,
+  `Item_Expiry_hours` datetime NOT NULL,
+  `Donation_status` varchar(255) NOT NULL
+);
+
+
+ALTER TABLE `current_donations`
+  ADD PRIMARY KEY (`Donation_id`);
+
+ALTER TABLE `current_donations`
+  MODIFY `Donation_id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+create table Event(
+     ngoReg varchar(255),
+     eventTitle varchar(255),
+     eventDate varchar(255),
+     contactName varchar(255),
+     contactEmail varchar(255),
+     eventDescription varchar(255),
+     EventTime varchar(255)
+  );
